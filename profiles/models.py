4e2ceafa388f50686cbb271d2787a96e12f4fa36
Jonny Saykosy <josayko@pm.me>
2022-06-06 15:42:55 +0200

    Create two apps: profiles and lettings

---
+++ profiles/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favorite_city = models.CharField(max_length=64, blank=True)
+
+    def __str__(self):
+        return self.user.username
