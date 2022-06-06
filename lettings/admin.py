67578f23359ed3daf93ac21cb7ef91c8fd4db69f
Jonny Saykosy <josayko@pm.me>
2022-06-06 16:45:15 +0200

    Done: db migration in 2 apps lettings and profiles

--- lettings/admin.py
+++ lettings/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Address, Letting
 
-# Register your models here.
+admin.site.register(Letting)
+admin.site.register(Address)
--- oc_lettings_site/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Address, Letting, Profile
-
-admin.site.register(Letting)
-admin.site.register(Address)
-admin.site.register(Profile)
