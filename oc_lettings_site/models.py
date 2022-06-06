412fcaea11d9f483be40854f70a4844c38f79e02
Jonny Saykosy <josayko@pm.me>
2022-06-06 11:22:50 +0200

    add black and isort modules for auto format

--- oc_lettings_site/models.py
+++ oc_lettings_site/models.py
@@ -1,6 +1,6 @@
-from django.db import models
-from django.core.validators import MaxValueValidator, MinLengthValidator
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinLengthValidator
+from django.db import models
 
 
 class Address(models.Model):
@@ -12,7 +12,7 @@ class Address(models.Model):
     country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
 
     def __str__(self):
-        return f'{self.number} {self.street}'
+        return f"{self.number} {self.street}"
 
 
 class Letting(models.Model):
