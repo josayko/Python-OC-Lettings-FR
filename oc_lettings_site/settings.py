bc7affaa95d21ee4ac3e1f0a439733fc6ffa070d
Jonny Saykosy <josayko@pm.me>
2022-06-15 15:28:44 +0200

    add a default value for DEBUG in settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", cast=bool)
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".herokuapp.com"]
 
