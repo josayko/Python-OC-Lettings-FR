8a1b5ce3ede6fffdd6e99aeb7f7e0e7d558e36d0
Jonny Saykosy <josayko@pm.me>
2022-06-23 11:57:07 +0200

    add whitenoise for handling static files when DEBUG=false

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -124,5 +125,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = ()
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 FIXTURE_DIRS = [os.path.join(BASE_DIR, "fixtures")]
